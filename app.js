1d4e83503694e33c2bbf67d3d68dc830d4078169
Shade555 <slevincordeiro@gmail.com>
2025-03-30 23:39:56 +0530

    fetch error handling

--- app.js
+++ app.js
@@ -4,11 +4,25 @@
 'use strict';
 
 const Myurl = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=FLg9C5EK6hjcjKVX7sGqdRl49KHbSF7TnRUjvQ0U';
+//new: code can now handle error in the url showing the url is invalid(if any changes are done in the above url)
+
+const handleErrors = (response) => {
+    if (response.ok) return response.json();
+    return response.json()
+        .then(response => {
+            throw new Error(response.error.message || response.error)
+        })
+};
+
+const handleSuccess = (data) => {
+    console.log(data)
+};
 
 const createRequest = (url) => {
     fetch(url)
-        .then((response) => response.json())      //can write console.log(response)
-        .then((data) => console.log(data))
+        .then((response) => handleErrors(response))      //can write console.log(response)
+        .then((data) => handleSuccess(data))
+        .catch((error) => console.error(error));
 };
 
 createRequest(Myurl);
