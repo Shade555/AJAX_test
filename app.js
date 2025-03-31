bdb8f25fab8816b1c5388aeb73502e0c2f104972
Shade555 <slevincordeiro@gmail.com>
2025-03-31 22:26:37 +0530

    to request data when an event occurs

--- app.js
+++ app.js
@@ -6,9 +6,12 @@
 const Myurl = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=FLg9C5EK6hjcjKVX7sGqdRl49KHbSF7TnRUjvQ0U';
 //code can now handle error in the url showing the url is invalid(if any changes are done in the above url)
 
+const Myurl_base = 'https://developer.nps.gov/api/v1/parks?parkCode='
+const Myurl_apikey = 'FLg9C5EK6hjcjKVX7sGqdRl49KHbSF7TnRUjvQ0U'
+
 const addressField = document.querySelector('#address');        //this version is vanilla javascript
 const $park = $('#park');                                       // this is in jquery
-//new: learning about query selectors and jquery 
+//learning about query selectors and jquery 
 
 const handleErrors = (response) => {
     if (response.ok) return response.json();
@@ -29,4 +32,17 @@ const createRequest = (url) => {
         .catch((error) => console.error(error));
 };
 
+const checkCompletion = () => {
+    if (addressField.value !== '' && $park.value !== ''){
+            const requestUrl = Myurl_base + addressField.value + '+' + $park.value + '+' +'&parkcode=acad' + '&api_key+' + Myurl_apikey;
+            createRequest(requestUrl);
+    }
+}
+//new: validation when data is entered, now the code can dynamically adjust data in a field so that it changes when input is changed
+//the above function checks if the field are not empty then it will create a request
+
 createRequest(Myurl);
+
+addressField.addEventListener('blur', checkCompletion);
+$park.addEventListener('blur', checkCompletion);
+//new: this two request will help in giving the data that is entered in the field
