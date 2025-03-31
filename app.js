34999299b92910793509f7dff17c93e72f84b217
Shade555 <slevincordeiro@gmail.com>
2025-03-31 18:36:22 +0530

    query selectors in DOM

--- app.js
+++ app.js
@@ -4,7 +4,11 @@
 'use strict';
 
 const Myurl = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=FLg9C5EK6hjcjKVX7sGqdRl49KHbSF7TnRUjvQ0U';
-//new: code can now handle error in the url showing the url is invalid(if any changes are done in the above url)
+//code can now handle error in the url showing the url is invalid(if any changes are done in the above url)
+
+const addressField = document.querySelector('#address');        //this version is vanilla javascript
+const $park = $('#park');                                       // this is in jquery
+//new: learning about query selectors and jquery 
 
 const handleErrors = (response) => {
     if (response.ok) return response.json();
